477a495ea8f44f165776dfb64f47ba0d52407af1
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-04 18:56:27 +0200

    Add New Messagge + Answer

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ Milestone 1:
 - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for,
   visualizzare nome e immagine di ogni contatto.
 
-*Milestone 2
+Milestone 2
 - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
   nome e immagine di ogni contatto.
 - Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
   visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione.
 - Click sul contatto mostra la conversazione del contatto cliccato.
 
-*Milestone 3 
-|- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene
-|  aggiunto al thread sopra, come messaggio verde.
-|- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta,
-|  che apparirà dopo 1 secondo.
+Milestone 3 
+- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene
+  aggiunto al thread sopra, come messaggio verde.
+- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta,
+  che apparirà dopo 1 secondo.
 
 *Milestone 4
 |- Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome
@@ -37,10 +37,15 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
+        selected: 'Marco Lanci',
+        newText: '',
+        newMessage: null,
+        newAnswer: null,
+        flag: null,
+
         user: {
             name: 'Leonardo Locatelli',
             avatar: '_8',
-            selected: '',
         },
         contacts: [
             {
@@ -130,7 +135,31 @@ const app = new Vue({
     },
     methods: {
         selectedContact(contatto) {
-            return this.user.selected = contatto.name;
-        }
+            this.selected = contatto.name;
+        },
+        addMessage() {
+            for (i = 0; i < this.contacts.length; i++) {
+                if (this.contacts[i].name === this.selected) {
+                    let now = dayjs();
+                    this.newMessage = {
+                        date: now.format("DD/MM/YYYY HH:mm:ss"),
+                        message: this.newText,
+                        status: 'sent',
+                    };
+                    this.contacts[i].messages.push(this.newMessage);
+                    this.answerMessage();
+                };
+            };
+            this.newText = '';
+        },
+        answerMessage() {
+            let now = dayjs();
+            this.newAnswer = {
+                date: now.format("DD/MM/YYYY HH:mm:ss"),
+                message: 'Ok',
+                status: 'received',
+            };
+            this.contacts[i].messages.push(this.newAnswer);
+        },
     },
 });
