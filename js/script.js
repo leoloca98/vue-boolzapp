271f86b6b485a10955fefa42bc2c449c6d1ae766
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-04 17:27:30 +0200

    Add v-for message

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ Milestone 1:
 *Milestone 2
 - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
   nome e immagine di ogni contatto.
-|- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
-|  visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione.
+- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
+  visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione.
 |- Click sul contatto mostra la conversazione del contatto cliccato.
 
 *Milestone 3 
@@ -40,6 +40,7 @@ const app = new Vue({
         user: {
             name: 'Leonardo Locatelli',
             avatar: '_8',
+            selected: '',
         },
         contacts: [
             {
